80470860f2674a2f0ce932beaae4e3754b533148
Kiefer <junlongchen20@gmail.com>
2025-06-11 19:09:44 +0800

    modified the designs

--- Algorithm.js
+++ Algorithm.js
@@ -1,13 +1,21 @@
-const display = document.getElementById("display-answer");
+const display = document.getElementById("show-answer");
 
 function appendToDisplay(input) {
-  display.value += input;
+  if (display.value === "0") {
+    display.value = input;
+  } else {
+    display.value += input;
+  }
 }
 
 function clearDisplay() {
-  display.value = "";
+  display.value = "0";
 }
 
 function calculate() {
-  display.value = eval(display.value);
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
