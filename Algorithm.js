2b19e833f8d7d8e449a2192dbb15962b3e611e79
Kiefer <junlongchen20@gmail.com>
2025-06-18 22:09:14 +0800

    Updated the calculate function sym x and /

--- Algorithm.js
+++ Algorithm.js
@@ -1,21 +1,46 @@
 const display = document.getElementById("show-answer");
+const calculatinputanswer = document.getElementById("calculateInput-answer");
+var calculateintput = "";
 
 function appendToDisplay(input) {
   if (display.value === "0") {
     display.value = input;
+    if (input == "x") {
+      input = "*";
+      calculateintput += input;
+      calculatinputanswer.value = calculateintput;
+    } else if (input == "÷") {
+      input = "/";
+      calculateintput += input;
+      calculatinputanswer.value = calculateintput;
+    } else {
+      calculateintput += input;
+      calculatinputanswer.value = calculateintput;
+    }
   } else {
     display.value += input;
+    if (input == "x") {
+      input = "*";
+      calculateintput += input;
+      calculatinputanswer.value = calculateintput;
+    } else if (input == "÷") {
+      input = "/";
+      calculateintput += input;
+      console.log = calculateintput;
+      calculatinputanswer.value = calculateintput;
+    } else {
+      calculateintput += input;
+      calculatinputanswer.value = calculateintput;
+    }
   }
 }
 
 function clearDisplay() {
   display.value = "0";
+  calculateintput = "";
+  calculatinputanswer.value = calculateintput;
 }
 
 function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "Error";
-  }
+  display.value = eval(calculateintput);
 }
